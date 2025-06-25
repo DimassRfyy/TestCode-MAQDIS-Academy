bb4e519a8aab94d7de2b94c35183667b00f56d92
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-06-25 15:31:36 +0700

    Endpoint post /data

--- index.js
+++ index.js
@@ -2,10 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// Endpoint GET /hello
 app.get('/hello', (req, res) => {
   res.json({
     status: 'success',
     message: 'welcome to Maqdis Academy'
+  });
+});
+
+// Endpoint POST /data
+app.post('/data', (req, res) => {
+  const data = req.body.data;
+
+  if (!data) {
+    return res.status(400).json({
+      status: 'data tidak boleh kosong'
+    });
+  }
+
+  const savedData = data;
+
+  res.json({
+    data: savedData,
+    status: 'berhasil'
   });
 });
 

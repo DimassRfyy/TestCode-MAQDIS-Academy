2d04a9940bdc6edc5fad24d271f255851cb31186
Dimas Rfyy <m.dimas.rafi12@gmail.com>
2025-06-25 14:35:26 +0700

    Tugas 4

---
+++ Tugas 4/looping.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "LOOPING PERTAMA\n";
+
+for ($i = 2; $i <= 20; $i += 2) {
+    echo "$i - I love coding\n";
+}
+
+echo "LOOPING KEDUA\n";
+
+for ($i = 20; $i >= 2; $i -= 2) {
+    echo "$i - I will become a mobile developer\n";
+}
